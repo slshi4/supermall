01373baf01ea3eae1d1f51ac30afc1f30c9195b8
slshi4 <2648230727@qq.com>
2022-06-19 01:19:20 +0800

    over

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from './store';
 
 import toast from 'components/common/toast';
+import FastClick from 'fastclick';
+import VueLazyLoad from 'vue-lazyload';
 
 Vue.config.productionTip = false
 
@@ -13,6 +15,15 @@ Vue.prototype.$bus = new Vue()
 //安装toast插件(手写的)
 Vue.use(toast);
 
+
+
+//解决移动端300ms延迟  attach附加
+FastClick.attach(document.body);
+
+// Vue.use(VueLazyLoad);
+Vue.use(VueLazyLoad,{
+  loading: require('assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -307,8 +307,8 @@ name: "Detail",
         this.message= '';
         },1500)*/
         console.log(this.$toast);
-        // this.$toast.show(res,2000);
-        this.$toast.show();
+        this.$toast.show(res,2000);
+        // this.$toast.show();
       });
 
     },
